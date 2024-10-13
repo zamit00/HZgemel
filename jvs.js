ca33f8f7d8e49c7f6a952bd9cc68ef3581cbf6d3
zamit00 <zamit00@gmail.com>
2024-10-13 15:19:52 +0300

    Update jvs.js

--- jvs.js
+++ jvs.js
@@ -338,9 +338,9 @@ function cleartesua() {
 
 
         window.addEventListener('message', (event) => {
-            if (event.origin !== 'https://zamit00.github.io/RibitDeRibit/') {
-                return;
-            }
+          //  if (event.origin !== 'https://zamit00.github.io/RibitDeRibit/') {
+           //     return;
+          //  }
             alert ( event.data.text);
 
             // Send a response back to the sender
